b3b291bf7b0f6342fc5b1874e752d1a6746a14b7
PTOM76 <ptms76@outlook.jp>
2025-06-08 12:06:44 +0900

    update Gradle and plugin configurations for compatibility with IntelliJ 2024.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    id("org.jetbrains.intellij") version "1.17.4"
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij.platform") version "2.3.0"
+    kotlin("jvm") version "1.9.25"
 }
 
 group = "net.pitan76"
-version = "0.0.5"
+version = "0.0.6"
 
 repositories {
     mavenCentral()
-    intellijPlatform {
-        defaultRepositories()
-    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-
-    intellijPlatform {
-        create("IC", "2024.2.5")
-        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
-
-        // Add necessary plugin dependencies for compilation here, example:
-        bundledPlugin("com.intellij.java")
-    }
 }
 
-intellijPlatform {
-    pluginConfiguration {
-        changeNotes = """
-            Fixes an issue
-    """.trimIndent()
-    }
+intellij {
+    version.set("2024.3.6")
+    type.set("IC")
+    plugins.set(listOf("java"))
+    updateSinceUntilBuild.set(false)
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
+        sourceCompatibility = "17" // Java 17を使用（21はまだ実験的）
+        targetCompatibility = "17"
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "21"
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
     }
 }
